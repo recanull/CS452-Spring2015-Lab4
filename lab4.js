8ebf9c3444bf1333ad1abbbbe469a6a82104f124
recanull <canullre@clarkson.edu>
2015-03-31 20:13:10 -0400

    Update lab4.js

--- lab4.js
+++ lab4.js
@@ -1,22 +1,214 @@
+/*Richard Evan Canull*/
+var canvas;
 var gl;
-var points;
-window.onload = function init()
-{
-var canvas = document.getElementById( "gl-canvas" );
-gl = WebGLUtils.setupWebGL( canvas );
-if ( !gl ) { alert( "WebGL isn't available" ); }
-//
-// Configure WebGL
-//
-gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-// Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-gl.useProgram( program );
-// Load the data into the GPU
-// Associate our shader variables with our data buffer
-render();
-};
-function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
+
+var numVertices  = 36;
+
+var pointsArray = [];
+var normalsArray = [];
+
+var texSize = 64;
+var texCoordsArray = [];
+var texture;
+var texCoord = [
+    vec2(0, 0),
+	vec2(0, 1),
+	vec2(1, 1),
+	vec2(1, 0)
+];
+
+
+var vertices = [
+        vec4( -0.1, -0.1,  0.03, 0.5 ), 
+        vec4( -0.7,  0.5,  0.5, 1.0 ), 
+        vec4(  0.1,  0.1,  0.03, 0.5 ), 
+        vec4(  0.7, -0.5,  0.5, 1.0 ),
+        vec4( -0.1, -0.1, -0.03, 0.5 ),
+        vec4( -0.7,  0.5, -0.5, 1.0 ),
+        vec4(  0.1,  0.1, -0.03, 0.5 ),
+        vec4(  0.7, -0.5, -0.5, 1.0 )
+    ];	
+
+var lightPos = vec4(0.0, 0.0, 0.5, 1.0 );
+var lightAmb = vec4(0.0, 0.4, 0.6, 1.0 );
+var lightDiff = vec4( 0.0, 1.0, 1.0, 1.0 );
+var lightSpec = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var matAmb = vec4( 1.0, 0.8, 1.0, 1.0 );
+var matDiff = vec4( 0.0, 0.0, 0.8, 1.0);
+var matSpec = vec4( 1.0, 0.8, 1.0, 1.0 );
+var materialShininess = 100.0;
+
+var ctm;
+var ambColor, diffColor, specColor;
+var modelView, projection;
+var viewerPos;
+var program;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = flag;
+var theta =[45, 45, 45];
+
+var check = 0;
+
+var thetaLoc;
+
+var flag = true;
+
+function quad(a, b, c, d ) {
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = cross(t1, t2);
+     var normal = vec3(normal);
+
+     pointsArray.push(vertices[a]); 
+     normalsArray.push(normal); 
+	 texCoordsArray.push(texCoord[0]);
+     pointsArray.push(vertices[b]); 
+     normalsArray.push(normal); 
+	 texCoordsArray.push(texCoord[1]);
+     pointsArray.push(vertices[c]); 
+     normalsArray.push(normal); 
+	   texCoordsArray.push(texCoord[2]);
+     pointsArray.push(vertices[a]);  
+     normalsArray.push(normal); 
+	 texCoordsArray.push(texCoord[0]);
+     pointsArray.push(vertices[c]); 
+     normalsArray.push(normal); 
+	 texCoordsArray.push(texCoord[3]);
+     pointsArray.push(vertices[d]); 
+     normalsArray.push(normal);   
+	 texCoordsArray.push(texCoord[2]); 
+}
+
+function shape(){
+	quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 6, 7 );
+	
+}
+
+window.onload = function init() {
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+    //  Load shaders and initialize attribute buffers
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    shape();
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+    
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+	var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+	gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vTexCoord );
+	// Texture
+
+	var image = document.getElementById("texImage");
+    configureTexture( image );
+
+    thetaLoc = gl.getUniformLocation(program, "theta"); 
+    viewerPos = vec3(0.0, 0.0, -20.0 );
+    projection = ortho(-1, 1, -1, 1, -100, 100);
+    
+    ambientProduct = mult(lightAmb, matAmb);
+    diffuseProduct = mult(lightDiff, matDiff);
+    specularProduct = mult(lightSpec, matSpec);
+	
+	document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
+       flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"),
+       flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), 
+       flatten(specularProduct) );	
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), 
+       flatten(lightPos) );
+    gl.uniform1f(gl.getUniformLocation(program, 
+       "shininess"),materialShininess);
+    gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),
+       false, flatten(projection));
+    render();
+}
+
+function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+    
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+}
+
+var render = function(){       
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	if(flag){  
+		if(check == 0){        
+			theta[axis] += 2.0;
+		} else if(check == 1){
+			theta[axis] -= 2.0;
+		}
+	}       
+    modelView = mat4();
+    modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0] ));
+    modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
+    gl.uniformMatrix4fv( gl.getUniformLocation(program,
+            "modelViewMatrix"), false, flatten(modelView) );
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );     
+    requestAnimFrame(render);
+}
+
+var curPressedKeys = {};
+
+function handleKeyDown(event) {
+    curPressedKeys[event.keyCode] = true;
+    if (event.keyCode == 37) {
+		check = 1;
+    	axis = yAxis;  
+    } else if (event.keyCode == 38) {
+		check = 1;
+    	axis = xAxis;  
+    } else if (event.keyCode == 39) {
+		check = 0;
+    	axis = yAxis;  
+    } else if (event.keyCode == 40) {
+		check = 0;
+    	axis = xAxis;  
+    }			
+}
+
+function handleKeyUp(event) {
+    curPressedKeys[event.keyCode] = false;
+	axis = flag;
 }
